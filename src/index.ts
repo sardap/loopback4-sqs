d3a517dd043fe89e1020da786d702e9f2c2154b2
Paul Sarda <paul.sarda@team.telstra.com>
2021-01-18 10:33:50 +1100

    added needed exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './keys';
 export * from './sqs.component';
 export * from './sqs.consumer';
 export * from './sqs.producer';
+export type { SQSMessage } from 'sqs-consumer';
+export type { ConfigurationOptions } from 'aws-sdk';
